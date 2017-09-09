2dd59ec24bf6bb3279fbe0c0c3ad5c7f63fafcb0
JuBin <jubinayoob369@gmail.com>
2017-09-09 13:52:41 +0530

    Create 04.Linked Lists: Detect a Cycle.java

---
+++ Cracking the Coding Interview/04.Linked Lists: Detect a Cycle.java
@@ -0,0 +1,17 @@
+boolean hasCycle(Node head) {
+  if (head == null) {
+    return false;
+  }
+  Set<Node> visited = new HashSet<Node>();
+  visited.add(head);
+  Node next = head.next;
+  while( next != null) {
+    if (visited.contains(next)) {
+      return true;  
+    }
+    visited.add(next);
+    next = next.next;
+  }
+
+  return false;
+}
