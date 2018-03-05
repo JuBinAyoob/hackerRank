ffe14352d582dfebe861dddb36dac90901157eb4
JuBin <jubinayoob369@gmail.com>
2018-03-05 19:39:42 +0530

    Update Day 2: Loops.js

--- 10 Days of Javascript/Day 2: Loops.js
+++ 10 Days of Javascript/Day 2: Loops.js
@@ -37,3 +37,37 @@ function main() {
     
     vowelsAndConsonants(s);
 }
+
+
+//Other approches
+//1
+/*
+// Returns a boolean denoting whether or not c is a vowel
+function isVowel(c) {
+    return 'aeiou'.indexOf(c) !== -1;
+}
+
+function vowelsAndConsonants(s) {
+    let v = [];
+    let c = [];
+    
+    // For each character in the string:
+    for (const e of s) {
+        if (isVowel(e)) {
+            v.push(e);
+        } else {
+            c.push(e);
+        }
+    }
+    
+    // Print vowels
+    for (let e of v) {
+        console.log(e);
+    }
+    
+    // Print consonants
+    for (const e of c) {
+        console.log(e);
+    }
+}
+*/
