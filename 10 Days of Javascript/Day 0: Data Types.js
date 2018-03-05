5cdf4ed9f60fbf746d8c45aad2d0b27d64b787f5
JuBin <jubinayoob369@gmail.com>
2018-03-05 18:03:54 +0530

    Update Day 0: Data Types.js

--- 10 Days of Javascript/Day 0: Data Types.js
+++ 10 Days of Javascript/Day 0: Data Types.js
@@ -35,6 +35,27 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     console.log(firstString+secondString);
 }
 
+//OR
+/*
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+
+    const firstInteger = 4;
+    
+    const firstDecimal = 4.0;
+    
+    const firstString = 'HackerRank ';
+    
+    console.log((firstInteger+ +(secondInteger)));
+    
+    
+    console.log((firstDecimal+ +(secondDecimal)));
+    
+    
+    console.log(firstString+secondString);
+    
+}
+*/
 function main() {
     const secondInteger = readLine();
     const secondDecimal = readLine();
